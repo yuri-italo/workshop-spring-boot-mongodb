1bd62a9ffa765d19653605a35cf81dddfe0d6925
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-09-21 10:49:13 -0300

    multiple criteria query

--- src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.educandoweb.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,5 +13,9 @@ import com.educandoweb.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ {date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
